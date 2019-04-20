7d100d0068c9d32df844343a7f4bff8d3f824a96
viswanath-kuruva <kkasi@zeomega.com>
2019-04-20 23:12:50 +0530

    Game App

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ class Config(object):
 	SECRET_KEY = 'strong-secret-key'
 	BASEDIR = basedir
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
